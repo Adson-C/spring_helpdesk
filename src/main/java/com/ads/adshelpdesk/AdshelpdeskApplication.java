17ea10c9685234fc9025c54262adbf04095ee105
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-02-15 09:51:03 -0300

    criando projeto

---
+++ src/main/java/com/ads/adshelpdesk/AdshelpdeskApplication.java
@@ -0,0 +1,13 @@
+package com.ads.adshelpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdshelpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdshelpdeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ads/adshelpdesk/AdshelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ads.adshelpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdshelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
