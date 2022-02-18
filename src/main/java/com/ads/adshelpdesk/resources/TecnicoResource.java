79b93283bac3d6c44edef0e07dc814b661d520d7
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-02-18 19:16:59 -0300

    Padrão DTO para Tecnico

--- src/main/java/com/ads/adshelpdesk/resources/TecnicoResource.java
+++ src/main/java/com/ads/adshelpdesk/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ads.adshelpdesk.domain.Tecnico;
+import com.ads.adshelpdesk.domain.dtos.TecnicoDTO;
 import com.ads.adshelpdesk.services.TecnicoService;
 
 @RestController
@@ -18,11 +19,11 @@ public class TecnicoResource {
 	private TecnicoService service;
 	// buscar por id
 	@GetMapping(value = "/{id}") // variavel de path
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
 		
 		Tecnico obj = service.findById(id);
 		
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 
 }
