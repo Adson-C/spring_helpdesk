91609be4154635d8e0c7900ce7cf80eb8871621c
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-02-22 22:43:45 -0300

    End-Point Delete para Tecnico

--- src/main/java/com/ads/adshelpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/ads/adshelpdesk/domain/dtos/TecnicoDTO.java
@@ -50,6 +50,7 @@ public class TecnicoDTO implements Serializable {
 		this.senha = obj.getSenha();
 		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
 		this.dataCraicao = obj.getDataCraicao();
+		addPerfis(Perfil.CLIENTE);
 	}
 
 	public Integer getId() {
--- src/main/java/com/ads/adshelpdesk/services/TecnicoService.java
+++ src/main/java/com/ads/adshelpdesk/services/TecnicoService.java
@@ -45,34 +45,43 @@ public class TecnicoService {
 		return repository.save(newObj);
 
 	}
-	
-public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
-		
+
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+
 		objDTO.setId(id);
 		Tecnico oldObj = findById(id);
 		validaPorCpfEmail(objDTO);
 		oldObj = new Tecnico(objDTO);
-		
+
 		return repository.save(oldObj);
-		
+
+	}
+
+	public void delete(Integer id) {
+
+		Tecnico obj = findById(id);
+		if (obj.getChamados().size() > 0) {
+
+			throw new DataIntegrityViolationException("Tecnico possui ordens de serviços e não pode ser deletado!");
+		}
+		repository.deleteById(id);
+
 	}
 
 	private void validaPorCpfEmail(TecnicoDTO objDTO) {
-		
+
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
 		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
-			
+
 			throw new DataIntegrityViolationException("CPF ja Cadastrado no Sistema!");
 		}
-		
+
 		obj = pessoaRepository.findByEmail(objDTO.getEmail());
-			if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
-			
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+
 			throw new DataIntegrityViolationException("E-mail ja Cadastrado no Sistema!");
-	
-			}
+
+		}
 	}
 
-	
-	
 }
