19dc99bb98decc1109942ef8115ad9eedb7ee233
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-02-24 22:47:15 -0300

    Authorization com JWT

--- src/main/java/com/ads/adshelpdesk/resources/TecnicoResource.java
+++ src/main/java/com/ads/adshelpdesk/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -49,6 +50,7 @@ public class TecnicoResource {
 	}
 	
 	// salvar 
+	@PreAuthorize("hasAnyRole('ADMIN')") // SÓ perfil admim pode acessar
 	@PostMapping
 	public ResponseEntity<TecnicoDTO> create(@Valid @RequestBody TecnicoDTO objDTO) {
 		Tecnico newObj = service.create(objDTO);
@@ -57,6 +59,7 @@ public class TecnicoResource {
 		
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')") // SÓ perfil admim pode acessar
 	// Atualizar
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<TecnicoDTO> update(@PathVariable Integer id,@Valid  @RequestBody TecnicoDTO objDTO) {
@@ -66,6 +69,8 @@ public class TecnicoResource {
 		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 	
+	
+	@PreAuthorize("hasAnyRole('ADMIN')") // SÓ perfil admim pode acessar
 	// deletar
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<TecnicoDTO> delete(@PathVariable Integer id){
--- src/main/java/com/ads/adshelpdesk/security/JWTUtil.java
+++ src/main/java/com/ads/adshelpdesk/security/JWTUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -23,6 +24,45 @@ public class JWTUtil {
 				.setExpiration(new Date(System.currentTimeMillis() + expiration))
 				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
 				.compact();
+	}
+
+	public boolean tokenValido(String token) {
+		
+		Claims claims = getclaims(token);
+		if (claims != null) {
+			
+			String username = claims.getSubject();
+			Date expeirationDate = claims.getExpiration(); // pega o tempo de ciracão do token
+			Date now = new Date(System.currentTimeMillis()); // pega a hora atual			
+			
+			if (username != null && expeirationDate != null && now.before(expeirationDate)) { // verifacar se o token ainda é valido
+				return true;
+				
+			}
+			
+		}
+		return false;
+	}
+
+	private Claims getclaims(String token) {
+		try {
+			
+			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+			
+			
+		} catch (Exception e) {
+			return null;
+		}
+		
+	}
+
+	public String getUsername(String token) {
+		
+		Claims claims = getclaims(token);
+		if (claims != null) {
+			return claims.getSubject();
+		}
+		return null;
 	}
 	
 }
